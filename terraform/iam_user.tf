71d593c35606c276d0137eb6a3c6f3f9c89c961d
Erkin Bektenov <ericbek72@yahoo.com>
2023-05-10 17:45:04 +0600

    first commit

---
+++ terraform/iam_user.tf
@@ -0,0 +1,30 @@
+resource "aws_iam_user" "my_user" {
+  name = "github-action-1"
+}
+
+resource "aws_iam_policy" "my_user_policy" {
+  name   = "github-action-policy"
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "ecr:GetAuthorizationToken"
+        ],
+        "Resource": "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "my_user_policy_attachment" {
+  name       = "github-action-policy-attachment"
+  users      = [aws_iam_user.my_user.name]
+  policy_arn = aws_iam_policy.my_user_policy.arn
+}
+
+resource "aws_iam_access_key" "my_access_key" {
+  user = aws_iam_user.my_user.name
+}
+
